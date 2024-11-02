9b95eb4da8141330ae64f86d9d1d3f400c06a324
abhinavv <49611458+abbbbbv@users.noreply.github.com>
2024-11-02 18:02:49 +0530

    Update kline01.py

--- kline01.py
+++ kline01.py
@@ -13,7 +13,7 @@ end_time = datetime.now().strftime("%d %b %Y %H:%M:%S")
 klines = client.futures_historical_klines_generator("BTCUSDT", Client.KLINE_INTERVAL_5MINUTE, start_time, end_time)
 
 # Write data to a text file
-with open('btc_5month_5min_data01.txt', 'w') as file:  # change file name is needed
+with open('btc_5month_5min_data01.txt', 'w') as file:  # change file name as needed
     for kline in klines:
         # Prepare the data point (Open Time, Open, High, Low, Close, Volume, Close Time)
         data_point = f"{kline[0]},{kline[1]},{kline[2]},{kline[3]},{kline[4]},{kline[5]},{kline[6]}"
